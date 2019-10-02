5582e0bfea11f77d67d51b83b393cd7cf25e05b1
Angel <aoanderson@sdsu.edu>
2019-10-02 07:50:13 -0700

    Next Track Button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,14 +14,37 @@
      player.playPause(nextSong);
   });
 
-  $('button#previous').on('click', function(){
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
-    const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex <= album.songs.length) { return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex -1;
+    if (prevSongIndex < -1) { return; }
 
-    const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
-
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
   });
+
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  })
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
+
+  setInterval( () => {
+    if(player.playstate !== 'playing') {return; }
+    const currentVolume = player.setVolume();
+    $('#volume-control input').val();
+  }, 1);
 }
